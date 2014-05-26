08a07c924a9ddf69627bc396d9ff79ea464312c4
Korolev <kunashir@list.ru>
2014-05-26 14:46:03 +0400

    add unicorn restart to deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,7 +56,15 @@ namespace :deploy do
     end
   end
 
-
+  task :restart do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D; fi"
+  end
+  task :start do
+    run "bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D"
+  end
+  task :stop do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+  end
 
   after :finishing, 'deploy:cleanup'
 
