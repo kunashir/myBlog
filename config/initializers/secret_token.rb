71a3c2d8e3a91e482a7390890e8327c6825d4657
Korolev <kunashir@list.ru>
2014-07-14 11:28:30 +0400

    change secret for session

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MyBlog::Application.config.secret_token = 'c03c54bf117c70006f86609e1c7d03b84b3bc90f1e20197cf0937ef189061e6e7e9bb1f5fa442fa4f4b9c032bc8e0a5de9e350966fcf83b3f247676c893ba15c'
+MyBlog::Application.config.secret_token = 'k03c54bf117c70006f86609e1c7d03b84b3bc90f1e20197cf0937ef189061e6e7e9bb1f5fa442fa4f4b9c032bc8e0a5de9e350966fcf83b3f247676c893ba15c'
